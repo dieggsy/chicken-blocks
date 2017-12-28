28816f9d7ebb6a3e6433a5723697524d1d80971f
dieggsy <dieggsy@protonmail.com>
2017-12-28 13:01:14 -0400

    Working wifi script.

---
+++ wifi.scm
@@ -0,0 +1,57 @@
+#!/usr/bin/csi -script
+(use (prefix dbus dbus:)
+     srfi-13)
+
+(dbus:auto-unbox-object-paths #t)
+(dbus:auto-unbox-variants #t)
+
+(define nm-props
+  (dbus:make-context
+   bus: dbus:system-bus
+   service: 'org.freedesktop.NetworkManager
+   path: '/org/freedesktop/NetworkManager
+   interface: 'org.freedesktop.DBus.Properties))
+
+;; connecion object paths
+(define active-connections
+  (vector->list
+   (car
+    (dbus:call nm-props "Get" "org.freedesktop.NetworkManager" "ActiveConnections"))))
+
+(define (make-connection-string path)
+  (let* ((context (dbus:make-context
+                   bus: dbus:system-bus
+                   service: 'org.freedesktop.NetworkManager
+                   path: path
+                   interface: 'org.freedesktop.DBus.Properties))
+         (type (car (dbus:call
+                     context
+                     "Get"
+                     "org.freedesktop.NetworkManager.Connection.Active" "Type")))
+         (icon (cond ((string-suffix? "wireless" type)
+                      " ")
+                     ((string-suffix? "ethernet" type)
+                      "<>")
+                     (else "?"))))
+    (let* ((id (car (dbus:call
+                     context
+                     "Get"
+                     "org.freedesktop.NetworkManager.Connection.Active"
+                     "Id"))))
+      (string-append icon " " id))))
+
+(define (main)
+  (let ((click (get-environment-variable "BLOCK_BUTTON")))
+    (when (and click (string= click "1"))
+      (system "networkmanager_dmenu &")))
+  (format #t
+          " ~a~%"
+          (let loop ((conn active-connections))
+            (if (null? conn)
+                ""
+                (string-append
+                 (make-connection-string (car conn))
+                 " "
+                 (loop (cdr conn)))))))
+
+(main)
