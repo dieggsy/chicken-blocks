32bb7d2b7ce306e72b38e0d9d6149edd2c73cec5
dieggsy <dieggsy@protonmail.com>
2017-12-29 01:06:26 -0400

    Add screen script.

---
+++ screen.scm
@@ -0,0 +1,21 @@
+#!/usr/bin/csi -script
+(use (prefix shell shell:)
+     s)
+
+(define (main)
+  (let* ((args (command-line-arguments))
+         (brightness (string->number
+                      (s-chomp (shell:capture "xbacklight -get"))))
+         ;; round to closest multiple of 5
+         (brightness (inexact->exact (* 5 (round (/ brightness 5)))))
+         (add (if (not (null? args))
+                  (cond ((and (< brightness 100) (string= (car args) "+")) 5)
+                        ((and (> brightness 0) (string= (car args) "-")) -5)
+                        (else 0))
+                  0)))
+    (let ((brightness (+ brightness add)))
+      (when (not (zero? add))
+        (system (format "xbacklight -set ~a > /dev/null 2>&1" brightness)))
+      (format #t "    ~a~%" brightness))))
+
+(main)
