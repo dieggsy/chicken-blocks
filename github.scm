387e04557ce9a6dc5a03bd89376434e1aac984f7
dieggsy <dieggsy@protonmail.com>
2018-01-22 11:28:48 -0500

    Add github notifications block.

---
+++ github.scm
@@ -0,0 +1,20 @@
+(use (prefix medea medea:)
+     (only (prefix http-client hc:) hc:with-input-from-request))
+
+(define access-token
+  (string-chomp
+   (read-all (format "~a/.polybar-ghub-token"
+                     (get-environment-variable "HOME")))))
+
+(define notifications-url
+  (format "https://api.github.com/notifications?access_token=~a"
+          access-token))
+
+(define (main)
+  (let ((data (hc:with-input-from-request
+               notifications-url
+               #f
+               medea:read-json)))
+    (if (zero? (vector-length data))
+        (printf "~%")
+        (printf "~a~%" (vector-length data)))))
