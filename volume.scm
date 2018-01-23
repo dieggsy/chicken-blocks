2584e03b81a52d58ce37c4ff293f16f5b494ae58
dieggsy <dieggsy@protonmail.com>
2018-01-22 21:48:52 -0500

    Add volume block

---
+++ volume.scm
@@ -0,0 +1,31 @@
+#!/usr/bin/csi -s
+(use scsh-process
+     irregex)
+
+(define args (command-line-arguments))
+
+(define (main)
+  (when (not (null? args))
+    (let (($1 (car args)))
+      (cond ((string= $1 "toggle")
+             (run (amixer -D pulse sset Master toggle)
+                  (> 1 /dev/null)
+                  (> 2 /dev/null)))
+            ((string= $1 "+")
+             (run (amixer -D pulse sset Master 5%+)
+                  (> 1 /dev/null)
+                  (> 2 /dev/null)))
+            ((string= $1 "-")
+             (run (amixer -D pulse sset Master 5%-)
+                  (> 1 /dev/null)
+                  (> 2 /dev/null))))))
+  (let* ((amixer-info (run/strings (amixer sget Master)))
+         (last-line (last amixer-info))
+         (regex (irregex '(: (submatch (+ numeric)) "%")))
+         (level (irregex-match-substring (irregex-search regex last-line) 1)))
+    (display level)
+    (newline)
+    (with-output-to-file "/tmp/ipc-polybar-simple"
+      (lambda () (display "hook:module/volume1")))))
+
+(main)
